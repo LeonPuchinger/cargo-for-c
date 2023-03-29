b1b6fad5bf3fc977681e6c6c16462a8200c65728
LeonPuchinger <lepu1590@gmail.com>
2023-03-30 00:50:05 +0200

    init cargo structure in existing directory

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,32 @@ function print_help() {
     console.log(myString);
 }
 
+/**
+ * Sets up cargo-for-c in an existing directory
+ */
+function init() {
+    try {
+        Deno.mkdirSync(".cargo-for-c/include", {recursive: true});
+        Deno.mkdirSync("src");
+        Deno.symlinkSync("src", ".cargo-for-c/include/crate-root", {type: "dir"});
+    } catch (error) {
+        if (!(error instanceof Deno.errors.AlreadyExists)) {
+            throw error;
+        }
+    }
+}
+
 function handle_cli() {
     const args = Deno.args;
     if (args.length < 1) {
         print_help();
         Deno.exit(1);
+    }
+    switch (args[0]) {
+        case "init":
+            return init();
+        default:
+            return print_help();
     }
 }
 
