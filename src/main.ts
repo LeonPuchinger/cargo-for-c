a4bd5f94a7190c6e08f9dd83c3e6051eecd47158
Leon Puchinger <24792398+LeonPuchinger@users.noreply.github.com>
2023-04-14 23:53:27 +0200

    Allow working with dependencies (#4)

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
 import { basename, join } from "https://deno.land/std/path/mod.ts";
 
 function print_help() {
-  const myString = `
+  const help_message = `
     cargo-for-c help:
     run: cargo-for-c <command> [options]
 
     commands:
+    - help: display this message
     - init: create new cargo instance in the CWD
     - new <name>: create new cargo instance in the dir called <name>
     - build: create executable from project in target/
         options:
         -r, --release: build a release instead of a debug executable (default)
+    - fetch: pull dependencies specified in cargo.toml
 `;
 
-    console.log(myString);
+    console.log(help_message);
 }
 
 /**
@@ -33,6 +35,7 @@ function init_cargo_dir(init_dir = "") {
         Deno.mkdirSync(join(init_dir, "target"));
         Deno.mkdirSync(join(init_dir, "src"));
         Deno.writeTextFileSync(join(init_dir, "src", "main.c"), "int main() {\n}");
+        Deno.writeTextFileSync(join(init_dir, "cargo.toml"), "[dependencies]\n");
         Deno.symlinkSync(
             join(init_dir, "src"),
             join(cargo_dir, "include/crate-root"),
@@ -116,9 +119,89 @@ function build_project(debug = true) {
 }
 
 /**
+ * Query list of dependencies from the cargo.toml file
+ * 
+ * @returns list of names/URLs of the dependencies
+ */
+function resolve_dependencies(): string[] {
+    assert_is_cargo_instance();
+    const cwd = Deno.cwd();
+    let cargo_file_contents = "";
+    try {
+        cargo_file_contents = Deno.readTextFileSync(join(cwd, "cargo.toml"));
+    } catch (error) {
+        if (error != Deno.errors.NotFound) {
+            throw error;
+        }
+        console.log("Could not fetch dependencies: no cargo.toml found");
+        Deno.exit(1);
+    }
+    if (cargo_file_contents.startsWith("[dependencies]")) {
+        cargo_file_contents = cargo_file_contents.replace("[dependencies]", "");
+    } else {
+        console.log("Could not fetch dependencies: no [dependencies] section found in cargo.toml");
+        Deno.exit(1);
+    }
+    const dependencies: string[] = [];
+    const dependency_line = /(\s*\n\s*(\S+))/g;
+    let dependency_match;
+    while ((dependency_match = dependency_line.exec(cargo_file_contents)) !== null) {
+        // successful match has three capture groups
+        if (dependency_match.length != 3) {
+            console.log('Error resolving dependency, skipping');
+            continue;
+        } 
+        dependencies.push(dependency_match[2]);
+    }
+    return dependencies;
+}
+
+/**
+ * Actually pull the dependencies from their respective remotes
+ * 
+ * @param dependencies a list of dependency URLs
+ */
+async function fetch_dependencies(dependencies: string[]) {
+    assert_is_cargo_instance();
+    const cwd = Deno.cwd();
+    for (const dependency of dependencies) {
+        // ignore the protocol in the URL of the dependency
+        const dependency_name = dependency.replace(/^\w+:\/\//, "");
+        const dependency_path = join(cwd, ".cargo-for-c", "include", dependency_name);
+        try {
+            Deno.statSync(dependency_path);
+            continue;
+        } catch (error) {
+            if (!(error instanceof Deno.errors.NotFound)) {
+                throw error;
+            }
+        }
+        console.log(`fetching ${dependency} ...`);
+        const git_process = Deno.run({
+            cmd: [
+                "git",
+                "clone",
+                dependency,
+                dependency_path,
+            ],
+            stderr: "piped",
+        });
+        const git_status = await git_process.status();
+        if (!git_status.success) {
+            console.log(`Error while trying to fetch ${dependency}, skipping...`);
+            console.log(`The git process returned with exit code ${git_status.code}`);
+            const stderr_output = (new TextDecoder).decode(await git_process.stderrOutput());
+            console.log(`The output is shown below (stderr):\n${stderr_output}`);
+            continue;
+        }
+        console.log("done!");
+    }
+}
+
+/**
  * Handle command line parameters and trigger the appropriate functions
  */
-function handle_cli() {
+async function handle_cli() {
     const args = Deno.args;
     if (args.length < 1) {
         print_help();
@@ -136,8 +219,12 @@ function handle_cli() {
             const release = ["-r", "--release"].includes(args[1]);
             return build_project(!release);
         }
+        case "fetch": {
+            const dependencies = resolve_dependencies();
+            return await fetch_dependencies(dependencies);
+        }
     }
     print_help();
 }
 
-handle_cli();
+await handle_cli();
